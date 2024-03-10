fd91fed0d4a587f21df39b17c14ce04acc003609
renatomf <renatomar76@gmail.com>
2024-03-10 20:26:06 -0300

    Individual Product Screen (Store)

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -1,5 +1,6 @@
-import { Product } from "@/types";
 import qs from "query-string";
+
+import { Product } from "@/types";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
@@ -11,7 +12,7 @@ interface Query {
 }
 
 const getProducts = async (query: Query): Promise<Product[]> => {
-  const url = qs.stringify({
+  const url = qs.stringifyUrl({
     url: URL,
     query: {
       colorId: query.colorId,
@@ -21,7 +22,7 @@ const getProducts = async (query: Query): Promise<Product[]> => {
     },
   });
 
-  const res = await fetch(URL);
+  const res = await fetch(url);
 
   return res.json();
 }
