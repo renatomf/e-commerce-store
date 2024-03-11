1247e074bcad56e85035406fbea267f893e059c1
renatomf <renatomar76@gmail.com>
2024-03-11 11:35:58 -0300

    Add to cart functionality (Store)

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -9,6 +9,7 @@ import { Product } from "@/types";
 import IconButton from "./icon-button";
 import Currency from "./currency";
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCardProps {
   data: Product;
@@ -17,6 +18,7 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({
   data
 }) => {
+  const cart = useCart();
   const previewModal = usePreviewModal()
   const router = useRouter();
 
@@ -28,6 +30,12 @@ const ProductCard: React.FC<ProductCardProps> = ({
     event.stopPropagation();
 
     previewModal.onOpen(data);
+  }
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    cart.addItem(data);
   }
 
   return (
@@ -50,7 +58,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
-              onClick={() => { }}
+              onClick={onAddToCart}
               icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
